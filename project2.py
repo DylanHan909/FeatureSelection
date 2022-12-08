a017e306485e34880074e702b72e5764acc8d466
Dylan Han <dylanhan909@gmail.com>
2022-12-08 04:14:59 -0800

    Fixed cross fold and implemented basic search

--- project2.py
+++ project2.py
@@ -9,35 +9,55 @@ def readData():
     #https://www.geeksforgeeks.org/find-the-number-of-rows-and-columns-of-a-given-matrix-using-numpy/
     return np.shape(data)[0], np.shape(data)[1], data
 
-def accuracy(data, features, feature_to_add): #k cross fold algorithm in lecture
+def cross_fold_accuracy(data, current_set, feature_to_add): #k cross fold algorithm in lecture
+    current_set = current_set.copy()
+    current_set.append(feature_to_add)
     number_correctly_classified = 0
-    rows = np.shape(data)[0]
-    for i in range(rows):
-        features_per_object = np.array
-        for j in data[i]:
-            features_per_object = np.append(features_per_object, j)
-        object_to_classify = np.delete(features_per_object, [0,1])
+    for i in range(len(data)):
+        object_to_classify = []
+        for features in current_set:
+            object_to_classify.append(data[i][features])
         label_object_to_classify = data[i][0]
         #print('Looping over i at the ' + str(i + 1) + ' location')
         #print('The ' + str(i + 1) + 'th object is in class ' + str(label_object_to_classify))
         #https://www.geeksforgeeks.org/python-infinity/
         nearest_neighbor_distance = float('inf')
         nearest_neightbor_location = float('inf')
-        for k in range(rows):
-            features_per_checked = np.array
-            for l in data[k]:
-                features_per_checked = np.append(features_per_checked, l)
-            object_to_check = np.delete(features_per_checked, [0,1])
+        for k in range(len(data)):
+            object_to_check = []
+            for features in current_set:
+                object_to_check.append(data[k][features])
             if k != i:
                 #print('Ask if ' + str(i + 1) + ' is nearest neighbor with ' + str(k + 1))
-                #https://www.geeksforgeeks.org/calculate-the-euclidean-distance-using-numpy/
-                distance = np.linalg.norm(object_to_classify - object_to_check)
+                #https://www.geeksforgeeks.org/python-math-dist-method/
+                distance = math.dist(object_to_classify, object_to_check)
                 if (distance < nearest_neighbor_distance):
                     nearest_neighbor_distance = distance
                     nearest_neightbor_location = k
                     nearest_neighbor_label = data[nearest_neightbor_location][0]
         print('Object ' + str(i + 1) + ' is class ' + str(label_object_to_classify))
         print('Its nearest neighbor is Object ' + str(nearest_neightbor_location + 1) + ' which is in class ' + str(nearest_neighbor_label))
+        if (label_object_to_classify == nearest_neighbor_label):
+            number_correctly_classified += 1
+    accuracy = number_correctly_classified/len(data)
+    return accuracy
+
+def search(data):
+    rows = np.shape(data)[0]
+    current_set_of_features = []
+    for i in range(rows - 1):
+        print('On the ' + str(i + 1) + ' level of the search tree')
+        feature_to_add_at_this_level = 0
+        best_so_far_accuracy = 0
+        for k in range(rows - 1):
+            if k not in current_set_of_features:
+                print('Consider adding the ' + str(k + 1) + ' feature')
+                accuracy = cross_fold_accuracy(data, current_set_of_features, k + 1)
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k
+        current_set_of_features.append(feature_to_add_at_this_level)
+        print('On level ' + str(i + 1) + ' i added feature ' + str(feature_to_add_at_this_level) + ' to current set')
 
 def main():
     print("Welcome to Dylan's Feature Selection Algorithm.")
@@ -45,6 +65,6 @@ def main():
     print(instances)
     print('This dataset has ' + str(features - 1) + ' features (not including the class attribute), with ' + str(instances) + ' instances.')
     #algorithm_type = input('Type the number of the algorithm you want to run.\n1) Forward Selection\n2) Backward Elimination\n')
-    accuracy(data, 0, 0)
+    search(data)
 
 main()
