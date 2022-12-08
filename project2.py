d69de192bdd38470b0982738b24eea88089ebbd7
Dylan Han <dylanhan909@gmail.com>
2022-12-07 23:31:09 -0800

    Skeleton

---
+++ project2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import copy
+
+def main():
+    print("Welcome to Dylan's Feature Selection Algorithm.")
+    filename = input('Type in the name of the file to test: ')
+
+    algorithm_type = input('Ty[pe the number of the algorithm you want to run.')
+main()
